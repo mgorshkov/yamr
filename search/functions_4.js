3fcc5a25888282a36fd9f5af0abfebca6e0ea74a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-10 11:00:47 +0000

    Deploy mgorshkov/yamr to github.com/mgorshkov/yamr.git:gh-pages

--- search/functions_4.js
+++ search/functions_4.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['get_5fboost_5farchive_5fname',['get_boost_archive_name',['../namespaceinstall__boost.html#a9d3e2ffb0c762602c2d5649b56ab1209',1,'install_boost']]],
   ['get_5fboost_5furl',['get_boost_url',['../namespaceinstall__boost.html#a3634fa8196ecd86dd648245a42518372',1,'install_boost']]],
-  ['get_5fextracted_5ffolder_5fname',['get_extracted_folder_name',['../namespaceinstall__boost.html#a5071bb93c46824c549d59f4e88e527e5',1,'install_boost']]],
-  ['getmaxlength',['GetMaxLength',['../structReduceFunctor.html#a8389189e3a4e5e7e32f6b2f543784cb2',1,'ReduceFunctor']]]
+  ['get_5fextracted_5ffolder_5fname',['get_extracted_folder_name',['../namespaceinstall__boost.html#a5071bb93c46824c549d59f4e88e527e5',1,'install_boost']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['operator_28_29',['operator()',['../structMapFunctor.html#ac588c1702a51cc15a64a59c26819a894',1,'MapFunctor::operator()()'],['../structReduceFunctor.html#a5415ca64e0352ddb93866f01ced96bfa',1,'ReduceFunctor::operator()()']]]
+  ['operator_28_29',['operator()',['../structMapFunctor.html#ac588c1702a51cc15a64a59c26819a894',1,'MapFunctor::operator()()'],['../structReduceFunctor.html#a8408b9d09e040e171de58595a7d3c1ea',1,'ReduceFunctor::operator()()']]]
 ];
